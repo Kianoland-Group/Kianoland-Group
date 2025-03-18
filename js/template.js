c3d9ca66d196e369102bdf91c450c500cbefe4b5
fatonyahmadfauzi <fatonyahmadfauzi@gmail.com>
2025-03-18 07:16:59 +0700

    test memindahkan floating whatsapp menjadi template

--- js/template.js
+++ js/template.js
@@ -1,8 +1,16 @@
 /// =================================================================================================================================================================================================
 /// ============ FLOATING WHATSAPP ==================================================================================================================================================================
 /// ================================================================================================================================================================================================= 
-fetch(window.location.origin + '/Kianoland-Group/template/whatsapp-button.html')
-  .then(response => response.text())
+const path = '/Kianoland-Group/template/whatsapp-button.html';
+console.log('Fetching:', path);
+
+fetch(path)
+  .then(response => {
+    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+    return response.text();
+  })
   .then(data => {
+    console.log('Fetched Data:', data);
     document.getElementById('whatsapp-button').innerHTML = data;
-});
+  })
+  .catch(error => console.error('Fetch Error:', error));
