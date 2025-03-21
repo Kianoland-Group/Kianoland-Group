16ae0f6eb74cd34dd954ee57ff6f6af77584b2cb
fatonyahmadfauzi <fatonyahmadfauzi@gmail.com>
2025-03-21 12:05:40 +0700

    back

--- js/script.js
+++ js/script.js
@@ -50,17 +50,3 @@ document.addEventListener('DOMContentLoaded', function() {
         progressBar.style.width = scrollPercentage + '%';
     });
 });
-
-document.addEventListener('DOMContentLoaded', () => {
-    const lazyImages = document.querySelectorAll('img[loading="lazy"]');
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                const img = entry.target;
-                img.src = img.dataset.src;
-                observer.unobserve(img);
-            }
-        });
-    });
-    lazyImages.forEach(img => observer.observe(img));
-});
