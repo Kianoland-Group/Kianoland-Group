bfcfec40bd5730562c57b7f6810f97fe0c299f19
fatonyahmadfauzi <fatonyahmadfauzi@gmail.com>
2025-10-07 09:39:00 +0800

    test deploy 11ty

--- blog/.eleventy.js
+++ blog/.eleventy.js
@@ -1,22 +1,19 @@
 import { DateTime } from "luxon";
 
-export default function (eleventyConfig) {
-  // Filter tanggal
+export default function(eleventyConfig) {
   eleventyConfig.addFilter("readableDate", (dateObj) => {
-    if (!dateObj) return "";
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("dd LLL yyyy");
   });
 
-  // ✅ Salin file dari luar folder blog ke hasil build
+  eleventyConfig.addPassthroughCopy("../css");
+  eleventyConfig.addPassthroughCopy("../images");
   eleventyConfig.addPassthroughCopy("../admin");
-  eleventyConfig.addPassthroughCopy({ "../css": "css" });
-  eleventyConfig.addPassthroughCopy({ "../images": "images" });
 
   return {
     dir: {
       input: ".",
       includes: "_includes",
-      output: "_site",
-    },
+      output: "_site"
+    }
   };
 }
