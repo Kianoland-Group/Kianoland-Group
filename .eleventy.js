e93c5ef9b9b4553bd0ec08816b767e882af0dd36
fatonyahmadfauzi <fatonyahmadfauzi@gmail.com>
2025-10-10 01:22:30 +0800

    test

--- .eleventy.js
+++ .eleventy.js
@@ -1,27 +1,33 @@
 import { DateTime } from "luxon";
 
 export default function (eleventyConfig) {
+  // Copy semua file statis yang dibutuhkan blog
+  eleventyConfig.addPassthroughCopy("blog/images");
+  eleventyConfig.addPassthroughCopy("blog/assets");
+  eleventyConfig.addPassthroughCopy("blog/css");
+  eleventyConfig.addPassthroughCopy("blog/js");
+
+  // Tambahkan filter tanggal
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     if (!dateObj) return "";
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("dd LLL yyyy");
   });
 
-  // Salin folder statis ke root hasil build
-  eleventyConfig.addPassthroughCopy({ "blog/images": "images" });
-  eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("js");
-  eleventyConfig.addPassthroughCopy("template");
+  // Koleksi untuk semua artikel blog
+  eleventyConfig.addCollection("blog", (collection) =>
+    collection.getFilteredByTag("blog").sort((a, b) => b.date - a.date)
+  );
 
+  // Konfigurasi direktori build khusus blog
   return {
     dir: {
       input: "blog",
       includes: "_includes",
-      layouts: "_includes/layouts",   // ✅ arahkan langsung ke folder layouts
-      output: ".", // hasil build langsung ke root project
+      layouts: "_includes/layouts",
+      output: "_site/blog"
     },
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
+    dataTemplateEngine: "njk"
   };
 }
